640816c4ef2cf10b7d7afc669b0dd106bec4bf04
Ilan Gitter <8359193+gitteri@users.noreply.github.com>
2020-10-25 18:03:19 -0400

    Improve testing and errors Return wrapped error and add a test case for it

--- pkg/convert/encoding.go
+++ pkg/convert/encoding.go
@@ -3,6 +3,7 @@ package convert
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"fmt"
 )
 
 // HexToB64 converts a hex string to a base64 string
@@ -10,7 +11,7 @@ func HexToB64(s string) (string, error){
 	var b64 string
 	hexBytes, err := hex.DecodeString(s)
 	if err != nil {
-		return b64, err
+		return b64, fmt.Errorf("decoding hex failed: %w", err)
 	}
 
 	b64 = base64.StdEncoding.EncodeToString(hexBytes)
--- pkg/convert/encoding_test.go
+++ pkg/convert/encoding_test.go
@@ -1,36 +1,46 @@
 package convert
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestHexToB64(t *testing.T) {
 	cs := []struct {
 		name        string
 		hex         string
 		expectedB64 string
-		expectedErr string
+		errLike     string
 	}{
 		{
 			name:        "nil hex",
 			hex:         "",
-			expectedErr: "Could not convert hex to b64",
+			expectedB64: "",
 		},
 		{
 			name:        "given cryptopals case",
 			hex:         "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
 			expectedB64: "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
 		},
+		{
+			name:        "invalid hex encoding",
+			hex:         "x*",
+			expectedB64: "",
+			errLike:     "decoding hex failed",
+		},
 	}
 
-	for _, c := range cs {
+	for _, cx := range cs {
+		c := cx
 		t.Run(c.name, func(t *testing.T) {
 			t.Parallel()
 			b64, err := HexToB64(c.hex)
-			if c.expectedErr != "" {
+			if c.errLike != "" {
 				if err == nil {
 					t.Fatal("expected error but got none")
 				}
-				if err.Error() != c.expectedErr {
-					t.Fatal("expected error", c.expectedErr, "but got err", err.Error())
+				if !strings.Contains(err.Error(), c.errLike) {
+					t.Fatal("expected error", c.errLike, "but got err", err.Error())
 				}
 			}
 
